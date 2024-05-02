3c9c48915de7f1b9019aff23cad427dd1321291f
ozgurruzgar <ozgurruzgar@users.noreply.github.com>
2024-05-02 10:11:08 +0300

    dev: Added CustomControllerBase and Course Service

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Controllers/CoursesController.cs
@@ -0,0 +1,67 @@
+﻿using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Services;
+using FreeCourse.Shared.ControllerBases;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesController : CustomBaseController
+    {
+        private readonly ICourseService _courseService;
+
+        public CoursesController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GeAll()
+        {
+            var result = await _courseService.GetAllAsync();
+
+            return CreateActionResultInstance(result);
+        }
+
+        [HttpGet("getbyid/{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var result = await _courseService.GetByIdAsync(id);
+
+            return CreateActionResultInstance(result);
+        }
+
+        [HttpGet("getbyuserid/{userid}")]
+        public async Task<IActionResult> GetByUserId(string userId)
+        {
+            var result = await _courseService.GetAllByUserId(userId);
+
+            return CreateActionResultInstance(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CourseCreateDto courseCreateDto)
+        {
+            var result = await _courseService.CreateAsync(courseCreateDto);
+
+            return CreateActionResultInstance(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(CourseUpdateDto courseUpdateDto)
+        {
+            var result = await _courseService.UpdateAsync(courseUpdateDto);
+
+            return CreateActionResultInstance(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var result = await _courseService.DeleteAsync(id);
+
+            return CreateActionResultInstance(result);
+        }
+    }
+}
---
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/CourseService.cs
@@ -0,0 +1,103 @@
+﻿using AutoMapper;
+using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Models;
+using FreeCourse.Services.Catalog.Settings;
+using FreeCourse.Shared.Dtos;
+using MongoDB.Driver;
+
+namespace FreeCourse.Services.Catalog.Services
+{
+    public class CourseService : ICourseService
+    {
+        private readonly IMongoCollection<Course> _courseCollection;
+        private readonly IMongoCollection<Category> _categoryCollection;
+        private readonly IMapper _mapper;
+
+        public CourseService(IMapper mapper, IDatabaseSettings databaseSettings)
+        {
+            var client = new MongoClient(databaseSettings.CoonnectionString);
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+
+            _courseCollection = database.GetCollection<Course>(databaseSettings.CourseCollectionName);
+            _categoryCollection = database.GetCollection<Category>(databaseSettings.CategoryCollectionName);
+            _mapper = mapper;
+        }
+
+        public async Task<Response<List<CourseDto>>> GetAllAsync()
+        {
+            var courses = await _courseCollection.Find(course => true).ToListAsync();
+            if (courses.Any())
+            {
+                foreach (var course in courses)
+                {
+                    course.Category = await _categoryCollection.Find(category => category.Id == course.CategoryId).FirstOrDefaultAsync();
+                }
+            }
+            else
+                courses = new List<Course>();
+
+            return Response<List<CourseDto>>.Success(_mapper.Map<List<CourseDto>>(courses), 200);
+        }
+
+        public async Task<Response<CourseDto>> GetByIdAsync(string id)
+        {
+            var course = await _courseCollection.Find<Course>(x => x.Id == id).FirstOrDefaultAsync();
+
+            if (course == null)
+            {
+                return Response<CourseDto>.Fail("Course Not Found.", 404);
+            }
+            course.Category = await _categoryCollection.Find(x => x.Id == course.CategoryId).FirstAsync();
+
+            return Response<CourseDto>.Success(_mapper.Map<CourseDto>(course), 200);
+        }
+
+        public async Task<Response<List<CourseDto>>> GetAllByUserId(string userId)
+        {
+            var courses = await _courseCollection.Find(course => course.UserId == userId).ToListAsync();
+
+            if (courses.Any())
+            {
+                foreach (var course in courses)
+                {
+                    course.Category = await _categoryCollection.Find(category => category.Id == course.CategoryId).FirstOrDefaultAsync();
+                }
+            }
+            else
+                courses = new List<Course>();
+
+            return Response<List<CourseDto>>.Success(_mapper.Map<List<CourseDto>>(courses), 200);
+        }
+
+        public async Task<Response<CourseDto>> CreateAsync(CourseCreateDto courseCreateDto)
+        {
+            var newCourse = _mapper.Map<Course>(courseCreateDto);
+            newCourse.CreatedDate = DateTime.Now;
+            await _courseCollection.InsertOneAsync(newCourse);
+
+            return Response<CourseDto>.Success(_mapper.Map<CourseDto>(newCourse), 200);
+        }
+
+        public async Task<Response<NoContent>> UpdateAsync(CourseUpdateDto courseUpdateDto)
+        {
+            var updatedCourse = _mapper.Map<Course>(courseUpdateDto);
+
+            var result = await _courseCollection.FindOneAndReplaceAsync(x => x.Id == courseUpdateDto.Id, updatedCourse);
+            if (result == null)
+            {
+                return Response<NoContent>.Fail("Course not found.", 404);
+            }
+            else
+                return Response<NoContent>.Success(204);
+        }
+
+        public async Task<Response<NoContent>> DeleteAsync(string id)
+        {
+            var result = await _courseCollection.DeleteOneAsync(x => x.Id == id);
+            if (result.DeletedCount > 0)
+                return Response<NoContent>.Success(204);
+            else
+                return Response<NoContent>.Fail("Course not found", 404);
+        }
+    }
+}
