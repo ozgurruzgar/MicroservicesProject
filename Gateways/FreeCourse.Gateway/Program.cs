12453696ff61b24ca5088d4377e87dcef7b67177
ozgurruzgar <ozgurruzgar@users.noreply.github.com>
2024-05-14 08:19:22 +0300

    Continue development

---
+++ Gateways/FreeCourse.Gateway/Program.cs
@@ -0,0 +1,23 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddJsonFile($"configuration.{builder.Environment.EnvironmentName}.json")
+    .AddEnvironmentVariables();
+});
+
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+await app.UseOcelot();
+
+app.Run();
