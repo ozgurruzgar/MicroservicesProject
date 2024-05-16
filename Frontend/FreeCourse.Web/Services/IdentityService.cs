e9b0b9f8456700ddd1edddf311197c78c2429e09
ozgurruzgar <ozgurruzgar@users.noreply.github.com>
2024-05-16 10:41:41 +0300

    Continue development

---
+++ Frontend/FreeCourse.Web/Services/IdentityService.cs
@@ -0,0 +1,61 @@
+﻿using FreeCourse.Shared.Dtos;
+using FreeCourse.Web.Models;
+using FreeCourse.Web.Services.Interfaces;
+using IdentityModel.Client;
+using Microsoft.Extensions.Options;
+
+namespace FreeCourse.Web.Services
+{
+    public class IdentityService : IIdentityService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IHttpContextAccessor _contextAccessor;
+        private readonly ClientSettings _clientSettings;
+        private readonly ServiceApiSettings _serviceApiSettings;
+
+        public IdentityService(HttpClient httpClient, IHttpContextAccessor contextAccessor, IOptions<ClientSettings> clientSettings, IOptions<ServiceApiSettings> serviceApiSettings)
+        {
+            _httpClient = httpClient;
+            _contextAccessor = contextAccessor;
+            _clientSettings = clientSettings.Value;
+            _serviceApiSettings = serviceApiSettings.Value;
+        }
+
+        public Task<TokenResponse> GetAccessTokenByRefreshToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RevokeRefreshToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Response<bool>> SignIn(SignInInput input)
+        {
+            var discovery = await _httpClient.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest
+            {
+                Address = _serviceApiSettings.BaseUrl,
+                Policy = new DiscoveryPolicy { RequireHttps = false }
+            });
+
+            if (!discovery.IsError)
+                throw discovery.Exception;
+            else
+            {
+                var passwordTokenRequest = new PasswordTokenRequest
+                {
+                    ClientId = _clientSettings.WebClientForUser.ClientId,
+                    ClientSecret = _clientSettings.WebClientForUser.ClientSecret,
+                    UserName = input.Email,
+                    Password = input.Password,
+                    Address = discovery.TokenEndpoint,
+                };
+
+                var token = await _httpClient.RequestPasswordTokenAsync(passwordTokenRequest);
+                //if (!token.IsError)
+
+            }
+        }
+    }
+}
---
+++ Frontend/FreeCourse.Web/Services/Interfaces/ServiceApiSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace FreeCourse.Web.Services.Interfaces
+{
+    public class ServiceApiSettings
+    {
+        public string BaseUrl { get; set; }
+        public string PhotoStockUrl { get; set; }
+    }
+}
