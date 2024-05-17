1f49982e32f765cc08c687e0e3f62174d200fb81
ozgurruzgar <ozgurruzgar@users.noreply.github.com>
2024-05-17 15:37:28 +0300

    Continue development

---
+++ Frontend/FreeCourse.Web/Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using FreeCourse.Web.Models;
+using FreeCourse.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IIdentityService _service;
+
+        public AuthController(IIdentityService service)
+        {
+            _service = service;
+        }
+
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(SignInInput signInInput)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            var response =  await _service.SignIn(signInInput);
+            if (!response.IsSuccessful)
+            {
+                response.Errors.ForEach(x =>
+                {
+                    ModelState.AddModelError(string.Empty, x);
+                });
+            }
+
+                return RedirectToAction(nameof(Index), "Home");
+
+            return View();
+        }
+    }
+}
